51f5479229ecc00ee41cef3534d7881f1aab6b7c
Ali <alimdv74.am@outlook.com>
2024-04-20 22:48:25 +0330

    add TodoItem model to django admin site

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import TodoItem
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ['label', 'done', 'created']
+    list_filter = ['done', 'created']
+    list_editable = ['done']
+    search_fields = ['label']
+    ordering = ['created']
