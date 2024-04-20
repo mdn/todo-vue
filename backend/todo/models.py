bf7eccea6a5c9e0a94d1712863df4642544f51d0
Ali <alimdv74.am@outlook.com>
2024-04-20 09:47:39 +0330

    create TodoItem model for todos

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class TodoItem(models.Model):
+    label = models.CharField(max_length=255)
+    done = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
