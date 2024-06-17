7b2ec59fa42ce29f7a19cc43abfaef8e7d1122c4
Dmitry Menov <18627661+mdmen@users.noreply.github.com>
2024-06-17 11:18:09 +0600

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'https://mdn.github.io/todo-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
